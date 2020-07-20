479be7452846c64de62e3c1e6338c5860e673d59
JEFFERSON ESTEVAM <jesbmail@gmail.com>
2020-07-20 17:17:12 -0300

    Update authMiddleware.js
    Updated!

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,35 +1,36 @@
-const JwtHelper = require("../helpers/JwtHelper");
+const jwt = require("jsonwebtoken");
+const { Status } = require("../helpers/StatusCode");
 
 class AuthMiddleware {
-
-    Jwt = JwtHelper.Jwt;
 
     async jwtValidation(req, res, next) {
         try {
             const Header = req.headers.authorization;
 
             if (!Header)
-                return res.status(401).json({ status: 8 });
+                return res.status(401).json(Status[6]);
 
             const parts = Header.split(" ");
 
             if (!parts.length === 2)
-                return res.status(401).json({ status: 7 });
+                return res.status(401).json(Status[6]);
 
             const [scheme, token] = parts;
 
             if (!/^Bearer$/i.test(scheme))
-                return res.status(401).json({ status: 7 });
+                return res.status(401).json(Status[6]);
 
-            this.Jwt.verify(token, authConfig.secret, (err, decoded) => {
-                if (err) throw new Error('Problem to generate token');
+            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+                if (err) throw new Error('Invalid token!');
                 req.userId = decoded.id;
             });
 
             return next();
 
-        } catch (err) {
-            return res.status(500).json({ status: 7, content: err });
+        } catch (error) {
+            let data = Status[6];
+            data.error = error.message;
+            res.status(400).json(data); 
         }
     }
 }
